00dd8c8b52d58e822e9fdbf3ab5d03b6de747c7e
fedeval <federico.valenza5@gmail.com>
2021-04-02 17:28:06 +0200

    feat: api calls to get mission data and latest photos for each rover

--- src/server/index.js
+++ src/server/index.js
@@ -12,17 +12,41 @@ app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
+// API Calls
+const roverNames = ['curiosity', 'opportunity', 'spirit']
 
-// example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
+
+// Get mission data for each rover
+const getRoverData = (rovers) => {
+  rovers.forEach(rover => {
+    app.get(`/${rover}` , async (req, res) => {
+      try {
+        let roverData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.API_KEY}`)
+          .then(res => res.json())
+        res.send({ roverData })
+      } catch (err) {
         console.log('error:', err);
-    }
-})
+      }
+    })
+  })
+}
+
+// Get latest photos for each rover
+const getRoverPhotos = (rovers) => {
+  rovers.forEach(rover => {
+    app.get(`/${rover}/photos`, async (req, res) => {
+      try {
+        let roverPhotos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`)
+        .then(res => res.json())
+        res.send({ roverPhotos })
+      } catch (err) {
+        console.log('error:', err);
+      }
+    })
+  })
+}
+
+getRoverData(roverNames)
+getRoverPhotos(roverNames)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
