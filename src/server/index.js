468c8b6ed80670efdfe4ce33a9ede886a8ac3103
fedeval <federico.valenza5@gmail.com>
2021-04-17 22:33:28 +0200

    chore: display favicon

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
+const favicon = require('serve-favicon')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
+app.use(favicon(path.join(__dirname,'..','public','assets','images','favicon.png')))
 
 // API Calls
 const roverNames = ['curiosity', 'opportunity', 'spirit']
